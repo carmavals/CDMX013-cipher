a60024ad1341c1eab947c2c2679298f4ea4fd83c
carmavals <mvcarbajals@pucp.edu.pe>
2022-10-03 22:26:20 -0500

    pagina con encoder

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import cipher from './cipher.js';
+let lo_que_dijo = document.getElementById('encode')
+let valor_lo_que_dijo = lo_que_dijo.value
+let posiciones = document.getElementById('offset')
+let valor_posicion = posiciones.value
+let codificar = cipher.encode(valor_posicion, valor_lo_que_dijo).toUpperCase()
+//agregar un div 
 
+if(codificar !== null) {
+    let new_div = document.createElement('div')
+    new_div.setAttribute('class', 'textoCodificado')
+    let div_codificador = document.getElementById('codificado')
+    new_div.innerHTML = `<span class='nuevoDivCodificador'>${codificar}</span>`
+    div_codificador.appendChild(new_div)
+} 
+
+console.log(codificar)
+console.log(valor_lo_que_dijo)
+console.log(valor_posicion)
 console.log(cipher);
--- src/sketch.js
+++ src/sketch.js
@@ -1,7 +1,27 @@
-function setup() {
-  createCanvas(400, 400);
-}
+let canvas
+let mic
+function windowResized() {
 
+  resizeCanvas(windowWidth, windowHeight)
+  console.log('funciona')
+}
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.position(0, 0)
+  canvas.style('z-index', '-1')
+  background(235, 152, 78);
+  // mic = new p5.AudioIn()
+  // mic.start()
+}
+// function mousePressed(){
+//   clear()
+// }
 function draw() {
-  background(220);
+  if (mouseIsPressed) {
+    stroke(125, 60, 152)
+    line(pmouseX, pmouseY, mouseX, mouseY)
+    strokeWeight(12)
+  }
+  //let vol = mic.getLevel()
+  //ellipse(width/2, height/2, vol*width)
 }
